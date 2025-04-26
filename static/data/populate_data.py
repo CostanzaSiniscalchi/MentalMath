fd88f1390fa78ab970d1a08cf1a283067ad19c7d
Costanza Siniscalchi <cs4014@columbia.edu>
2025-04-26 13:38:42 -0400

    all data

--- static/data/populate_data.py
+++ static/data/populate_data.py
@@ -1,21 +1,19 @@
 import json
 import os
+from collections import defaultdict
 
-
-def populate_data(unit1, unit2, unit3):
-    # load data drom jsons
-    unit_1 = json.loads(unit1)
-    unit_2 = json.loads(unit2)
-    unit_3 = json.loads(unit3)
-    units = [unit_1, unit_2, unit_3]
+def populate_data(units):
     id = 0
-    data = {}
-    # create a dictionary with the data
+    data = {
+        "1": {"easy": defaultdict(dict), "medium": defaultdict(dict), "hard": defaultdict(dict), "test": defaultdict(dict)},
+        "2": {"easy": defaultdict(dict), "medium": defaultdict(dict), "hard": defaultdict(dict), "test": defaultdict(dict)},
+        "3": {"easy": defaultdict(dict), "medium": defaultdict(dict), "hard": defaultdict(dict), "test": defaultdict(dict)}
+    }
 
     for i, unit in enumerate(units):
         for level in unit:
             for question in unit[level]:
-                data[str(i)][level][str(id)] = {
+                data[str(i+1)][level][str(id)] = {
                     "problem": question["problem"],
                     "answer": question["answer"],
                     "solution_gif": question["solution_gif"]
@@ -25,8 +23,20 @@ def populate_data(unit1, unit2, unit3):
 
 if __name__ == "__main__":
     # Example usage
-    unit1_json = 'static/data/multiply11.json'
-    unit2_json = 'static/data/squared5.json'
-    unit3_json = 'static/data/midpoint.json'
-    data = populate_data(unit1_json, unit2_json, unit3_json)
-    print(data)
+    unit1_json = 'multiply11.json'
+    unit2_json = 'square5.json'
+    unit3_json = 'midpoint_problems.json'
+    
+    units = []
+    for unit in [unit1_json, unit2_json, unit3_json]:
+        with open(unit, encoding='utf-8') as f:
+            units.append(json.load(f))
+
+    data = populate_data(units)
+    
+    # Save to JSON file
+    output_path = 'full_data.json'
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    print(f"Data successfully written to {output_path}")
