ea39cf9e17eeabb0c71f0c7a341557fbeade6a7e
NookRitzia <Edward.Stan.04@gmail.com>
2025-04-26 03:01:32 -0400

    quiz page tweaks
    on load, input box is automatically hovered on. one submitted, next question is automatically hovered on.
    fixed timer not aligned with question
    added solid border #888 around the progress bar.

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -20,6 +20,26 @@ FormSubmitted = false;
             ${data.correct}
           </span>
         `;
+
+        feedbackBox = document.getElementById('feedback')
+        const result = data // loaned code
+        console.log(result)
+        feedbackBox.className = result.correct
+          ? "alert alert-success"
+          : "alert alert-warning";
+        feedbackBox.innerText = result.message;
+        feedbackBox.style.display = "block";
+
+        // Optionally disable the form so users can't re-submit
+        form.querySelector("input[name='user-answer']").disabled = true;
+        form.querySelector("button[type='submit']").disabled = true;
+
+        // Enable "Next Question" button
+        nextButton.disabled = false;
+
+
+
+
       });
     });
   });
@@ -27,7 +47,7 @@ FormSubmitted = false;
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('quiz-form');
   const submitBtn = form.querySelector('button[type="submit"]');
-  const inputField = form.querySelector('input[name="user_answer"]');
+  const inputField = form.querySelector('input[name="user-answer"]');
 
   form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -42,9 +62,13 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     .then(res => res.json())
     .then(data => {
+     /*
       document.getElementById('feedback').innerHTML = `
         <div class="alert alert-${data.status} mt-3">${data.message}</div>
       `;
+        */
+      document.getElementById('nextButton').focus();
+
     })
     .catch(err => {
       console.error('Error:', err);
@@ -62,9 +86,9 @@ $(document).ready(function () {
 function submitFormElements(submitBtn, inputField) { // function to clear buttons and show correct feedback text
     // Disable form controls after first submission
     $('#nextButton').show();
-    submitBtn.style.visibility = 'hidden';
+    //submitBtn.style.visibility = 'hidden';
     submitBtn.disabled = true;
-    inputField.disabled = true;
-
+    //inputField.disabled = true;
 }
+
 
